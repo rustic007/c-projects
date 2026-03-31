3e4d95caac67231f414068ba9361f201b3719f3d
Ruslan <ruslanospanov0306@gmail.com>
2026-03-31 16:21:52 +0500

    Modified the cat.c

--- cat.c
+++ cat.c
@@ -1,6 +1,12 @@
 #include "cat.h"
+
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 // you should have struct contains: bools and filename
 // 3 functions main, print_file and parse_args
 // main contains: the enough argc and argv provided also the exta functions
@@ -8,108 +14,128 @@
 // -b -n with strcmp print_file: chccks not empty, writes lines based onl flags
 // and counts line numbers.
 
-Options parse_args(int argc, char *argv[]) {
+Options parse_args(int argc, char* argv[]) {
   Options opts = {false, false, false, false, false, NULL};
-
-  for (int i = 1; i < argc; i++) {
-    if (argv[i][0] == '-') {
-      for (size_t j = 1; j < strlen(argv[i]); j++) {
-        switch (argv[i][j]) {
-        case 'n':
-          opts.show_end_lines = true;
-          break;
-        case 'b':
-          opts.blank_lines = true;
-          break;
-        case 'e':
-          opts.show_end_lines = true;
-          break;
-        case 't':
-          opts.show_tab = true;
-          break;
-        case 's':
-          opts.squaze_lines = true;
-          break;          
-        default:
-          break;
-        }
-      }
-    } else {
-      opts.filename = argv[i];
+  int opt;
+  while ((opt = getopt(argc, argv, "nbeEtTs")) != -1) {
+    switch (opt) {
+      case 'n':
+        opts.show_line_nums = true;
+        break;
+      case 'b':
+        opts.blank_lines = true;
+        break;
+      case 'E':
+        opts.show_end_lines = true;
+        break;
+      case 'e':
+        opts.show_end_lines = true;
+        opts.show_tab = true;
+        break;
+      case 't':
+        opts.show_tab = true;
+        break;
+      case 'T':
+        opts.show_tab = true;
+        break;
+      case 's':
+        opts.squaze_lines = true;
+        break;
+      default:
+        break;
     }
   }
+
+  if (optind < argc) {
+    opts.FILE_NAME = argv[optind];
+  }
+
   return opts;
 }
 
-void print_file(const char *filename, bool show_line_num, bool blank_lines,
-                bool show_end_lines, bool show_tab, bool squaze_lines) {
-  FILE *file = fopen(filename, "r");
+void print_file(const char* FILE_NAME, const Options* opts) {
+  int fd = open(FILE_NAME, O_RDONLY);
 
-  if (file == NULL) {
-    printf("There is no such file: %s", filename);
+  if (fd < 0) {
+    perror(FILE_NAME);
     return;
   }
 
+  char buffer[4096];
   char line[1024];
+  size_t line_len = 0;
   int line_num = 1;
   bool prev_is_blank = false;
+  ssize_t bytes_read;
 
-  while (fgets(line, sizeof(line), file) != NULL) {
+  while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0) {
+    for (ssize_t i = 0; i < bytes_read; i++) {
+      unsigned char ch = (unsigned char)buffer[i];
 
-    bool is_blank = (line[0] == '\n');
+      if (ch == '\n') {
+        bool is_blank = (line_len == 0);
 
-    if (squaze_lines && is_blank && prev_is_blank) {
-      continue;
-    }
+        if (opts->squaze_lines && is_blank && prev_is_blank) {
+          prev_is_blank = true;
+          continue;
+        }
 
-    prev_is_blank = is_blank;
+        prev_is_blank = is_blank;
 
-    if (blank_lines && line[0] != '\n') {
-      printf("%d:", line_num);
-      line_num++;
-    }
+        if (opts->blank_lines && !is_blank) {
+          printf("%6d\t", line_num);
+          line_num++;
+        }
 
-    if (show_line_num) {
-      printf("%d:", line_num);
-      line_num++;
-    }
+        if (opts->show_line_nums) {
+          printf("%6d\t", line_num);
+          line_num++;
+        }
 
-    size_t len = strlen(line);
-    if (len > 0 && line[len - 1] == '\n') {
-      line[len - 1] = '\0';
-    }
+        for (size_t j = 0; j < line_len; j++) {
+          unsigned char c = (unsigned char)line[j];
+          if (opts->show_tab && c == '\t') {
+            printf("^I");
+          } else {
+            printf("%c", c);
+          }
+        }
 
-    for (int i = 0; line[i] != '\0'; i++) {
-      if (show_tab && line[i] == '\t') {
-        printf("^I");
+        if (opts->show_end_lines) {
+          printf("$");
+        }
+
+        printf("\n");
+        line_len = 0;
+      } else if (ch == '\r' && opts->show_tab) {
+        printf("^M");
       } else {
-        printf("%c", line[i]);
+        if (line_len < sizeof(line) - 1) {
+          line[line_len++] = ch;
+        }
       }
     }
-
-    if (show_end_lines) {
-      printf("$");
-    }
-
-    printf("\n");
   }
-  fclose(file);
+
+  if (bytes_read < 0) {
+    perror("read");
+  }
+  close(fd);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   if (argc < 2) {
-    printf("Usage: %s [-n] [-b] <filename>", argv[0]);
+    fprintf(stderr, "Usage: %s [-n] [-b] <filename>", argv[0]);
     return 1;
   }
 
   Options opts = parse_args(argc, argv);
 
-  if (opts.filename == NULL) {
-    printf("Error occured to find the file: %s", argv[0]);
+  if (opts.FILE_NAME == NULL) {
+    fprintf(stderr, "Error: no filename provided");
     return 1;
   }
 
-  print_file(opts.filename, opts.show_line_nums, opts.blank_lines,
-             opts.show_end_lines, opts.show_tab, opts.squaze_lines);
+  print_file(opts.FILE_NAME, &opts);
   return 0;
 }
--- cat.h
+++ cat.h
@@ -9,12 +9,11 @@ typedef struct {
   bool show_end_lines;
   bool show_tab;
   bool squaze_lines;
-  const char *filename;
+  const char *FILE_NAME;
 } Options;
 
 Options parse_args(int argc, char *argv[]);
 
-void print_file(const char *filename, bool show_line_num, bool blank_lines,
-                bool show_end_lines, bool show_tab, bool squaze_lines);
+void print_file(const char *FILE_NAME, const Options *opts);
 
 #endif
